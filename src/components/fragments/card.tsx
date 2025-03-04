42e9f03ce07d271935b78bc1195f6a45b420601a
Yudann <yudannnnn80@gmail.com>
2025-03-05 02:01:07 +0700

    chore: add className proops

--- src/components/fragments/card.tsx
+++ src/components/fragments/card.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 
-const Card = ({ children }: { children: React.ReactNode }) => {
+const Card = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
   return (
-    <div className="flex text-[24px] md:text-4xl font-bold flex-col items-center justify-center p-5 bg-black-background border-[2px] border-black-border rounded-[24px] text-white text-center">
+    <div
+      className={` flex text-[24px] md:text-4xl font-bold flex-col justify-center p-5 bg-black-background border-[2px] border-black-border rounded-[24px] text-white ${className}`}
+    >
       {children}
     </div>
   );
