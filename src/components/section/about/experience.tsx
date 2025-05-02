993ff4198c0936ef9c606d11e054a54d5f16a023
Yudann <yudannnnn80@gmail.com>
2025-05-02 15:53:49 +0700

    feat: add bem

--- src/components/section/about/experience.tsx
+++ src/components/section/about/experience.tsx
@@ -31,8 +31,8 @@ export default function ExperienceSection() {
       {/* JOKI WEBSITE */}
       <div className="flex flex-col items-start justify-center w-full pt-4 border-t border-black-border">
         <Link
-          href="/project"
-          target="_blank"
+          href="/projects"
+          // target="_blank"
           className="flex items-center justify-center font-semibold gap-2 py-1 px-3 border-[2px] bg-[#262626] border-black-border rounded-[12px] hover:bg-green-background transition-all duration-200"
         >
           <span>Joki Website</span>
@@ -41,12 +41,37 @@ export default function ExperienceSection() {
         <h1 className="font-semibold my-2">
           Frontend Web Developer • 2023 - Current
         </h1>
-        <p>
+        <p className="mb-4">
           Providing custom website development services for students and
           professionals, specializing in academic projects, final assignments
           (Penelitian Ilmiah), and other web-related tasks. I ensure that every
           project is well-structured, optimized, and tailored to the client`s
           needs, using Next.js, React.js, and modern web technologies.
+        </p>
+      </div>
+
+      {/* WEBSITE BEM FIKTI UG 2025 */}
+      <div className="flex flex-col items-start justify-center w-full pt-4 border-t border-black-border">
+        <Link
+          href="https://bemfikti-gunadarma.web.id/"
+          target="_blank"
+          className="flex items-center justify-center font-semibold gap-2 py-1 px-3 border-[2px] bg-[#262626] border-black-border rounded-[12px] hover:bg-green-background transition-all duration-200"
+        >
+          <span>BEM FIKTI UG </span>
+          <GrFormNextLink size={25} className="-rotate-45" />
+        </Link>
+        <h1 className="font-semibold my-2">
+          Frontend Web Developer • 2024 - Current
+        </h1>
+        <p>
+          Developing the official website for BEM FIKTI Universitas Gunadarma as
+          the frontend developer. I used <strong>Next.js</strong> and{" "}
+          <strong>Tailwind CSS</strong> to build a modern, responsive, and
+          user-friendly platform that helps students easily access information
+          about campus events, organizational activities, and other resources.
+          This project gave me the opportunity to implement clean UI/UX
+          practices and deliver a fast, accessible experience tailored for
+          university students.
         </p>
       </div>
     </Card>
