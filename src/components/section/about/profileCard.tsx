e7807761b1d833bedef0e211a382d5da15611081
Yudann <yudannnnn80@gmail.com>
2025-05-30 22:11:04 +0700

    feat: add skelton

--- src/components/section/about/profileCard.tsx
+++ src/components/section/about/profileCard.tsx
@@ -1,20 +1,33 @@
+"use client";
+
 import Card from "@/components/fragments/card";
 import Image from "next/image";
 import { FaUniversity } from "react-icons/fa";
 import { IoLocationOutline } from "react-icons/io5";
+import { useState } from "react";
 
 export default function ProfileCard() {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return (
     <Card className="items-start text-left">
-      <div className="flex mb-5 items-center justify-center  aspect-square">
+      {/* Image with Skeleton Loading */}
+      <div className="flex mb-5 items-center justify-center aspect-square relative">
+        {!imageLoaded && (
+          <div className="absolute inset-0 w-full h-full rounded-[12px] bg-gray-700 animate-pulse"></div>
+        )}
         <Image
           src="/yudan-about.jpeg"
           alt="yudan about"
-          width={2000}
-          height={2000}
-          className="w-full rounded-[12px] lg:grayscale hover:grayscale-0 transition-all duration-200"
+          width={1000}
+          height={1000}
+          className={`w-full rounded-[12px] lg:grayscale hover:grayscale-0 transition-all duration-200 ${
+            !imageLoaded ? "opacity-0" : "opacity-100"
+          }`}
+          onLoadingComplete={() => setImageLoaded(true)}
         />
       </div>
+
       <h1 className="mb-4 text-2xl font-bold">Hey again 👋</h1>
       <p className="text-base font-normal">
         I`m Yudan, a Front-End Developer who thrives on building smooth,
@@ -24,7 +37,7 @@ export default function ProfileCard() {
         deeper into modern web technologies while constantly refining my craft.
         Let`s turn great ideas into reality together!
       </p>
-      <div className="flex items-start justify-center gap-3 text-sm mt-5 text-[#b3b3b3] ">
+      <div className="flex items-start justify-center gap-3 text-sm mt-5 text-[#b3b3b3]">
         <div className="flex items-center gap-2 py-2 px-3 border-[2px] bg-[#262626] border-black-border rounded-[12px]">
           <IoLocationOutline size={20} />
           <span>Tangerang, Indonesia</span>
